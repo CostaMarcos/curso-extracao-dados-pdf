50ebde1dcfcc60345949e9a073f35bd55de5f7f3
marcos.costa <marcos.costa@lge.com>
2022-11-09 11:14:46 -0400

    codigo

---
+++ README.md
@@ -0,0 +1,2 @@
+## Instalação
+    pip install pytesseract==0.3.10 PyMuPDF==1.20.1 opencv-python==4.6.0.66 pandas
---
+++ main.py
@@ -0,0 +1,47 @@
+import pytesseract
+import re
+import fitz
+from PIL import Image
+import os
+import cv2 as cv2
+import pandas as pd
+
+# Exemplo 1: Retirando informações de imagem
+def exemplo_1():
+    return (pytesseract.image_to_string('teste.png'))
+
+# Exemplo 2: Buscando padrões no texto por Regex
+# https://regexr.com/
+def exemplo_2(text):
+    result = re.search(pattern="([0-9])\w+", string=text)
+    return result[0]
+
+# Exemplo 3: Lendo arquivos PDFs
+def exemplo_3():
+    local = os.getcwd()
+    doc = fitz.open(f"{local}//pdf_exemplo.pdf")
+    i = 0
+    #convertendo paginas do PDF para imagens
+    for page in doc:
+        i += 1
+        pix = page.get_pixmap(dpi=200)
+        pix.save(f"./images_pdf/{i}.png")
+        
+    # Coletando imagens da pasta
+    images = os.listdir("./images_pdf")
+    
+    # lendo cada imagem da pasta
+    tags = pd.read_excel("coordenadas.xlsx")
+    for image in images:
+        img = cv2.imread(f"./images_pdf/{image}")
+        for i in tags.index:
+            box = img[
+                tags['y1'][i]:tags['y2'][i],
+                tags['x1'][i]:tags['x2'][i]
+                ]
+            text = pytesseract.image_to_string(box)
+            print(f"{tags['tag'][i]}: {text}")
+
+
+if __name__ == "__main__":
+    exemplo_3()
